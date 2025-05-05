359b4cedcea7464a78f31e94a62e13bb6db1cf96
DavidTeaTunjo <dteatino@tunjosolutions.co>
2025-05-05 14:07:00 -0500

    eliminacion

--- js/main.js
+++
@@ -1,9 +0,0 @@
-  window.addEventListener('scroll', function () {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 10) {
-      nav.classList.add('scrolled');
-    } else {
-      nav.classList.remove('scrolled');
-    }
-  });
-
